88d2120aaae747d22831ba64114dc2cf2ee89534
tony_ojeda <tojeda96@gmail.com>
2019-08-11 11:23:06 -0500

    var, formas de nombrar una variable

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,4 +1,9 @@
-let name = prompt('¿Cual es tu nombre?');
-let age = prompt('¿Cual es tu edad?');
+// var , let , const
+var nombre = 'tony',
+    apellido = 'ojeda';
 
-document.getElementById('app').innerHTML = `Bienvenido ${name} de ${age} años`;
+// mas de una palabra
+var primerNombre = 'Tony'; // camelcase  recomendad en js
+var primer_nombre = 'Tony'; // underscore
+var PrimerNombre = 'Tony'; // pascalcase
+var primernombre = 'tony';
