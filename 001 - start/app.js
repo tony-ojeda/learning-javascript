c7ad9725a015ebe8b164bc3e16787f9582be05a0
tony_ojeda <tojeda96@gmail.com>
2019-08-12 06:37:44 -0500

    functions in string's

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,9 +1,16 @@
-let mensaje;
+// functions in js
 
-mensaje = 'Y entonces dije: \'estoy cool!!\' ';
-mensaje = 'JAVA' + 'SCRIPT';
-let aprendiendo = 'Aprendiendo',
-    tecnologia = 'JS';
+let nombre, mensaje;
 
-//console.log(mensaje);
-console.log(`${aprendiendo} ${tecnologia}`);
+nombre = "tony";
+mensaje = "Estoy aprendiendo JS "
+
+console.log(nombre.length);
+console.log(nombre.toUpperCase()); // Convierte a mayuscula
+console.log(nombre.toLowerCase()); //  Convierte a minuscula
+console.log(mensaje.substring(6, 10)); // muestra el texto desde , hasta
+console.log(mensaje.slice(6, 10)); // corta  el texto
+console.log(mensaje.split(' ')); // separa el string en arrays, segun el filtro
+console.log(mensaje.replace('JS', 'TypeScript')); // reemplaza un valor especifo de la cadena
+console.log(mensaje.includes('JS')); // permite buscar un dato en especifico (true o false)
+console.log(mensaje.repeat(3)); // repite el dato
