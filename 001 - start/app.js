bc71d0e77d7202205c205000160e6ed039a2c74a
tony_ojeda <tojeda96@gmail.com>
2019-08-12 13:53:19 -0500

    Tipos de datos: primitivos y de referencia

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,25 +1,19 @@
-// functions in 3s
+// tipos de datos
+// javascrip es un lenguaje del tipo dinamico donde no se especifican el tipo de dato
 
-const numero1 = 30,
-    numero2 = 20,
-    numero3 = 20.20,
-    numero4 = .1020,
-    numero5 = -3;
+let valor;
 
-let resultado;
+// Tipos de datos primitivos
+valor = 'Cadena de texto';
+valor = 20;
+valor = true;
+valor = null;
+valor = undefined;
+valor = Symbol('Simbolo');
 
-// Valor pi
-resultado = Math.PI;
+// Tipos de dato por referencia
+valor = { nombre: 'tony', apellido: 'ojeda' };
+valor = [1, 2, 3, 4];
+valor = new Date();
 
-// redondeo
-resultado = Math.round(2.5); // 2.4 es 2, 2.5 es 3
-resultado = Math.ceil(2.2); // redonde hacia arriba
-resultado = Math.floor(2.2); // redonde hacia abajo
-resultado = Math.sqrt(49); // raiz cuadrada
-resultado = Math.abs(numero5); // retorna el valor absoluto
-resultado = Math.pow(2, 3); // # a la potencia x
-resultado = Math.min(3, 6, 5, 9, 7, 8); // retorna el minimo valor ya sea en decimal
-resultado = Math.max(3, 6, 5, 9, 7, 8); // retorna el maximo valor ya sea en decimal
-resultado = Math.floor(Math.random() * 10);
-
-console.log(resultado);
+console.log(typeof valor);
