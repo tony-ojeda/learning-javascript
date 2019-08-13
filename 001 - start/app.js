c4474c2ac4f52d1750dcd1a1ea94b127bb5d9619
tony_ojeda <tojeda96@gmail.com>
2019-08-13 05:45:41 -0500

    arrays in js

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,32 +1,46 @@
-// template lirerals
-const producto1 = 'Pizza',
-    precio1 = 30,
-    producto2 = 'Hamburguesa',
-    precio2 = 40;
+// arreglos en js
+const numeros = [10, 20, 30, 50];
+//console.table(numeros);
 
-let html;
+// Areglo de string (metodo alternativo)
+const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril');
+//console.log(meses.length);
 
-// forma antigua
-// html = '<ul>' +
-//     '<li>Order: ' + producto1 + '</li>' +
-//     '<li>Precio: ' + precio1 + '</li>' +
-//     '<li>Orden: ' + producto2 + '</li>' +
-//     '<li>Precio: ' + precio2 + '</li>' +
-//     '</ul>';
+// añadir un arreglo
+meses[4] = 'Mayo';
+meses.push('Junio');
 
-// forma nueva
-html = `
-       <ul>
-        <li> Order: ${producto1} </li>
-        <li> Precio: ${precio1} </li>
-        <li> Orden: ${producto2} </li>
-        <li> Precio: ${precio2} </li>
-        <li> Total: ${total(precio1,precio2)} </li>
-       </ul>
-       `;
+// Añadir al inicio del arreglo
+meses.unshift('Mes 0');
 
-function total(precio1, precio2) {
-    return precio1 + precio2;
-}
+// Eliminar un elemento ubicado en la ultima posición de un arreglo
+meses.pop();
 
-document.getElementById('app').innerHTML = html;
+// Eliminar un elemento ubicado al inicio del arreglo
+meses.shift();
+
+// Eliminar desde un rango en especifico
+meses.splice(2, 1);
+
+// Revertir
+meses.reverse();
+
+// encontrar un elemento en el arreglo
+//console.log(meses.indexOf('Abril'));  // Se puede realizar para buscar en una lista de productos
+
+// unir arreglo con otro
+let arreglo1 = [1, 2, 3],
+    arreglo2 = [4, 5, 6];
+//console.log(arreglo1.concat(arreglo2));
+
+// Ordenar un arreglo
+const frutas = ['Platano', 'Manzana', 'Fresa', 'Naranja', 'Anis'];
+frutas.sort();
+//console.log(frutas);
+
+// Ordenar numeros
+arreglo1 = [3, 9, 91, 92, 23, 45, 21, 56, 1, 100, 10];
+arreglo1.sort(function(x, y) {
+    return x - y; // menor a mayo    return y - x; de mayor a menor
+});
+console.table(arreglo1);
