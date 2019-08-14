01d67cd8e63731fd024e1dbb54b5d5b68cb4f23e
tony_ojeda <tojeda96@gmail.com>
2019-08-14 07:28:12 -0500

    MAP in arrays of objectx and for in

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,14 +1,26 @@
-// Recorrer un arreglo con forEach
-const pendientes = ['Tarea', 'Comer', 'Proyecto', 'Aprender JS'];
+// Map para recorrer un arreglo de objetos
+const carrito = [
+    { id: 1, producto: 'Libro' },
+    { id: 2, producto: 'Camisa' },
+    { id: 3, producto: 'Guitarra' },
+    { id: 4, producto: 'Disco' }
+];
 
-// for comun
-/*
-for (let i = 0; i < pendientes.length; i++) {
-    console.log(pendientes[i]);
-}
-*/
-
-// forEach
-pendientes.forEach(function(pendiente, index) {
-    console.log(`${index + 1} - ${pendiente}`);
+const lista_por_nombre_carrito = carrito.map(function(carrito) {
+    return carrito.producto;
 });
+
+//console.dir(lista_por_nombre_carrito);
+
+const automovil = {
+    modelo: 'Camaro',
+    motor: 6.1,
+    anio: 1999,
+    marca: 'Chevrole'
+};
+
+for (let auto in automovil) {
+    console.log(`${auto} : ${automovil[auto]}`);
+}
+
+console.dir(automovil);
