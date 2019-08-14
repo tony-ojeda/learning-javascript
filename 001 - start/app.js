1af4cf7e96bb628b3ee64520434e0ff9823f53a2
tony_ojeda <tojeda96@gmail.com>
2019-08-14 04:52:55 -0500

    function expression

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,29 +1,7 @@
-// function declaration en js
-// Creas una función para que solucione un pequeño problema
+// function expression
 
-/**
-function saludar() {
-    console.log('Holiii!!!');
-}
-
-saludar();
-*/
-
-/*
-function saludarA(nombre) {
-    console.log(`Hola ${nombre}, como estas!!`);
-}
-
-saludarA("tony");
-saludarA("Juan");
-saludarA("Jose");
-*/
-
-function sumar(a, b) {
+const suma = function(a, b) {
     return a + b;
 }
 
-let suma;
-suma = sumar(1, 2);
-suma = sumar(3, 4);
-console.log(suma);
+console.log(suma(1, 3));
