524dc9a35a9d7b14ffed40111442a4ed54410eca
tony_ojeda <tojeda96@gmail.com>
2019-08-20 11:20:21 -0500

    modificando atributos html con getElementById

--- 001 - start/universidad/js/app.js
+++ 001 - start/universidad/js/app.js
@@ -1,33 +1,13 @@
-// Eliminar de Local Storage
-//localStorage.clear();
+// getElementById
 
-let elmento;
+let encabezado;
 
-elemento = document;
-elemento = document.all;
-elemento = document.all[10];
-elemento = document.head;
-elemento = document.body;
-elemento = document.domain;
-elemento = document.URL;
-elemento = document.characterSet;
-elemento = document.forms;
-elemento = document.forms[0];
-elemento = document.forms[0].id;
-elemento = document.forms[0].className;
-elemento = document.forms[0].classList;
-elemento = document.forms[0].classList[0];
-elemento = document.images[2]
-elemento = document.images[2].getAttribute('src');
+encabezado = document.getElementById('encabezado');
+encabezado.style.background = '#333';
+encabezado.style.color = '#fff';
+encabezado.style.padding = '20px';
 
-// Scripts
-elemento = document.scripts;
-elemento = document.images;
-let imagenes = document.images;
-let imagenesArr = Array.from(imagenes);
+// Cambiar el texto
+encabezado.textContent = 'Los mejores Cursos'
 
-imagenesArr.forEach(function(imagen) {
-    console.log(imagen);
-});
-
-console.log(imagenesArr);
+console.log(encabezado);
