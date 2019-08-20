117557cdc44f13d0ae2522b77ad1fc4098a7508c
tony_ojeda <tojeda96@gmail.com>
2019-08-20 13:50:46 -0500

    Query Selector in one element js

--- 001 - start/universidad/js/app.js
+++ 001 - start/universidad/js/app.js
@@ -1,13 +1,13 @@
-// getElementById
+// query selector
+// usas la misma sinstaxis de css
+// Tambien puede seleccionar clases, de devuelve la primera clase que encuentra
 
-let encabezado;
+const encabezado = document.querySelector('#encabezado');
 
-encabezado = document.getElementById('encabezado');
+// Aplicar CSS
 encabezado.style.background = '#333';
 encabezado.style.color = '#fff';
 encabezado.style.padding = '20px';
-
-// Cambiar el texto
-encabezado.textContent = 'Los mejores Cursos'
+encabezado.textContent = 'Los mejores cursos';
 
 console.log(encabezado);
