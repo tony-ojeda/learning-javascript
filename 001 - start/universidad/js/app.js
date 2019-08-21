7e0f199f064b0863c0c0116d184fea9054fb7ea6
tony_ojeda <tojeda96@gmail.com>
2019-08-21 05:44:49 -0500

    Selection fo multiple elements with querySelectorAll

--- 001 - start/universidad/js/app.js
+++ 001 - start/universidad/js/app.js
@@ -1,27 +1,8 @@
-// Selección de multiples elementos
-// let enlaces = document.getElementsByClassName('enlace')[3];
-
-// // CSS
-// enlaces.style.background = '#333';
-// enlaces.textContent = 'Nuevo enlace';
-
-// console.log(enlaces);
-
-
-// Combinando Query Selector
-// const listaEnlaces = document.querySelector('#principal')
-//     .getElementsByClassName('enlace');
-
-// console.log(listaEnlaces);
-
-
-// Obteniendo Elementos por nombre etiquetas
-const links = document.getElementsByTagName('a');
-// links[18].style.color = 'red';
-// links[18].textContent = 'Nuevo enlace';
-// console.log(links);
-
-let enlaces = Array.from(links);
-enlaces.forEach(function(enlace) {
-    console.log(enlace.textContent);
+// selección multiple con querySelector
+const enlaces = document.querySelectorAll('#principal a:nth-child(odd)');
+enlaces.forEach(function(impares) {
+    impares.style.backgroundColor = 'red';
+    impares.style.color = 'white';
 });
+
+console.log(enlaces);
