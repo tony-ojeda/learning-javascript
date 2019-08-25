b1f9815111695720c1a4ab6abf9375bd7c719742
tony_ojeda <tojeda96@gmail.com>
2019-08-25 06:14:45 -0500

    event: click, dblclick, mouseenter, mouseleave, mouseover, mouseout, mouseout, mousedown, mouseup, mousemove

--- 001 - start/universidad/js/app.js
+++ 001 - start/universidad/js/app.js
@@ -1,13 +1,36 @@
-// Event Listener click in js
-document.querySelector('#submit-buscador').addEventListener('click', ejecutarBoton);
+// variables
+const encabezado = document.querySelector('#encabezado');
+const enlaces = document.querySelectorAll('.enlace');
+const boton = document.querySelector('#vaciar-carrito');
 
-function ejecutarBoton(evento) {
-    evento.preventDefault();
-    let elemento;
-    elemento = evento;
-    elemento = evento.target;
-    elemento = evento.target.id;
-    elemento = evento.target.className;
-    elemento = evento.target.innerText;
-    console.log(elemento);
+// Click
+//boton.addEventListener('click', obtenerEvento);
+
+// Doble Click
+// boton.addEventListener('dblClick', obtenerEvento);
+
+// Mouse Enter: Se activa el evento cuando el cursor se acerca al elemento escogido
+// boton.addEventListener('mouseenter', obtenerEvento);
+
+// Moouse Leave: Se activa el evento cuando el cursor se retira del elemento
+// boton.addEventListener('mouseleave', obtenerEvento);
+
+// Mouse Over: Se activa el evento cuando el cursor esta sobre el elemento
+//boton.addEventListener('mouseover', obtenerEvento);
+
+// Mouse Out: Similar a mouse leave
+// boton.addEventListener('mouseleave', obtenerEvento);
+
+//Mouse Down: Similar mouse enter
+// boton.addEventListener('mousedown', obtenerEvento);
+
+// Mouse Up: Se activa el evento cuando despues de un click, sueltas el click
+// boton.addEventListener('mouseup', obtenerEvento);
+
+// Mouse Move: Se activa por cualquier movimiento del cursor dentro del elemento
+encabezado.addEventListener('mousemove', obtenerEvento);
+
+
+function obtenerEvento(event) {
+    console.log(`EVENTO: ${event.type}`);
 }
