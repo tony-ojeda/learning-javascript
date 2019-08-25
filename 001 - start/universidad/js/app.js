7e358b164e8fd86b51a694abeb3eccc6b03b2072
tony_ojeda <tojeda96@gmail.com>
2019-08-24 23:26:32 -0500

    event click js

--- 001 - start/universidad/js/app.js
+++ 001 - start/universidad/js/app.js
@@ -1,26 +1,13 @@
-// Elminar elementos
-const enlaces = document.querySelectorAll('.enlace');
-const navegacion = document.querySelector('#principal');
+// Event Listener click in js
+document.querySelector('#submit-buscador').addEventListener('click', ejecutarBoton);
 
-//navegacion.removeChild(enlaces[0])
-enlaces[0].remove();
-
-// Obtener enlaces
-const primerLi = document.querySelector('.enlace');
-
-let elemento;
-
-// obtener una clase de CSS
-elemento = primerLi.className;
-elemento = primerLi.classList.add('nueva-clase');
-elemento = primerLi.classList.remove('nueva-clase');
-elemento = primerLi.classList;
-
-// Atributos
-elemento = primerLi.getAttribute('href');
-primerLi.setAttribute('href', 'httt://facebook.com');
-primerLi.setAttribute('data-id', 20);
-elemento = primerLi.hasAttribute('data-id'); // verifica si el atributo existe
-primerLi.removeAttribute('data-id');
-
-elemento = primerLi;
+function ejecutarBoton(evento) {
+    evento.preventDefault();
+    let elemento;
+    elemento = evento;
+    elemento = evento.target;
+    elemento = evento.target.id;
+    elemento = evento.target.className;
+    elemento = evento.target.innerText;
+    console.log(elemento);
+}
