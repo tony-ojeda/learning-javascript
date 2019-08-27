63dc0e0bc40a99723aca023b2013ee9f78618d7a
tony_ojeda <tojeda96@gmail.com>
2019-08-27 05:32:00 -0500

    event Bubbling - StopPropagation

--- 001 - start/universidad/js/app.js
+++ 001 - start/universidad/js/app.js
@@ -1,32 +1,20 @@
-// Eventos Teclado
-const busqueda = document.querySelector('#buscador');
+// Event Bubbling
 
-// keydown: Se activa el evento por cualquier cambio
-// busqueda.addEventListener('keydown', obtenerEvento);
+const card = document.querySelector('.card');
+const infoCurso = document.querySelector('.info-card');
+const agregarCarrito = document.querySelector('.agregar-carrito');
 
-// keyup: Se activa el evento por cada vez aue sueltas la tecla
-// busqueda.addEventListener('keyup', obtenerEvento);
+card.addEventListener('click', function(event) {
+    console.log('Click en Card');
+    event.stopPropagation();
+});
 
-// keypress: Evento se activa por cada tecla presionada
-//busqueda.addEventListener('keypress', obtenerEvento);
+infoCurso.addEventListener('click', function(event) {
+    console.log('Click en Info Curso');
+    event.stopPropagation();
+});
 
-// focus: Se activa el evento cuando el elemento este apuntado o enfocado
-// busqueda.addEventListener('focus', obtenerEvento);
-
-// blur: Se activa cuando pierdes el enfoque del elemento previo
-// busqueda.addEventListener('blur', obtenerEvento);
-
-// cut: cortar
-// busqueda.addEventListener('cut', obtenerEvento);
-
-// copy: copiar
-// busqueda.addEventListener('copy', obtenerEvento);
-
-// input: Captura todos los eventos ya antes mecionados
-busqueda.addEventListener('input', obtenerEvento);
-
-
-function obtenerEvento(event) {
-    document.querySelector('#encabezado').innerText = busqueda.value;
-    console.log(`EVENTO: ${event.type}`);
-}
+agregarCarrito.addEventListener('click', function(event) {
+    console.log('Click en Agregar a Carrito');
+    event.stopPropagation();
+});
