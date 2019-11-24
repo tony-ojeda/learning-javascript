a006a4216ebcf453fde9b8dc6a397bca44d1a927
tony_ojeda <tojeda96@gmai.com>
2019-11-23 23:13:35 -0500

    valadita data input at form

--- 004 - email/js/app.js
+++ 004 - email/js/app.js
@@ -9,10 +9,52 @@ eventListeners();
 
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', inicioApp);
+    // campos del formulario
+    email.addEventListener('blur', validarCampo);
+    asunto.addEventListener('blur', validarCampo);
+    mensaje.addEventListener('blur', validarCampo);
 }
 
 // funciones
 function inicioApp() {
     // deshabilitar el envio
     btnEnviar.disabled = true;
+}
+
+// Valida que el campo tenga algo escrito
+function validarCampo() {
+    // Se valida la longitud del texto y que no este vacio
+    validarLongitud(this);
+
+    // Validar unicamente el email
+    if (this.type === 'email') {
+        validarEmail(this);
+    }
+
+    let errores = document.querySelectorAll('error');
+
+    if (email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
+        if (errores.length === 0) btnEnviar.disabled = false;
+    }
+}
+
+function validarLongitud(campo) {
+    if (campo.value.length > 0) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    } else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
+}
+
+function validarEmail(campo) {
+    const mensaje = campo.value;
+    if (mensaje.indexOf('@') !== -1) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    } else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
 }
