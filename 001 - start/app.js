5f697f96e3e6a98b09a7a88080beff0375b4eb79
tony_ojeda <tojeda96@gmai.com>
2019-11-24 12:09:36 -0500

    use of classes in EMACScript6

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,18 +1,38 @@
-// Object Create 
-const Cliente = {
-    imprimirSaldo: function() {
-        return `hola ${this.nombre} tu saldo es ${this.saldo}`;
-    },
-    retirarSaldo: function(retiro) {
+// EMACScript6
+class Cliente {
+    constructor(nombre, apellido, saldo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.saldo = saldo;
+    }
+
+    imprimirSaldo() {
+        return `Hola ${this.nombre}, tu saldo es de : ${this.saldo}`;
+    }
+
+    tipoCliente() {
+        let tipo;
+        if (this.saldo > 10000) {
+            tipo = 'Gold';
+        } else if (this.saldo > 5000) {
+            tipo = 'Platinum';
+        } else {
+            tipo = 'Normal';
+        }
+        return tipo;
+    }
+
+    retirarSaldo(retiro) {
         return this.saldo -= retiro;
+    }
+
+    static bienvenida() {
+        return `Bienvenido al cajero`;
     }
 }
 
-// Crear el objeto
-const tony = Object.create(Cliente);
-tony.nombre = 'Tony';
-tony.saldo = 500;
+// const tony = new Cliente('Tony', 'Ojeda', 110000);
+// tony.retirarSaldo(3000);
+// console.log(tony.imprimirSaldo());
 
-tony.retirarSaldo(200);
-
-console.log(tony.imprimirSaldo());
+console.log(Cliente.bienvenida());
