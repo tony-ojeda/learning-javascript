483c6a7c84e65f0cedb8d7c98df96945885c195a
tony_ojeda <tojeda96@gmai.com>
2019-11-23 22:08:28 -0500

    create const for use at project

---
+++ 004 - email/js/app.js
@@ -0,0 +1,10 @@
+// variables
+const email = document.getElementById('email');
+const asunto = document.getElementById('asunto');
+const mensaje = document.getElementById('mensaje');
+const btnEnviar = document.getElementById('enviar');
+
+// event listener
+
+
+// funciones
