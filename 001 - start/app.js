dc26525786d449feedb95d97e77e93d267abdec4
tony_ojeda <tojeda96@gmai.com>
2019-11-24 06:53:25 -0500

    create object alternative

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,11 +1,10 @@
-// Object literal
-
-const cliente = {
-    nombre: 'Juan',
-    saldo: 200,
-    tipoCliente: () => {
+// Metodo alternativo
+function Cliente(nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+    this.tipoCliente = () => {
         let tipo;
-        if (this.saldo > 1000) {
+        if (this.saldo > 2000) {
             tipo = 'Gold';
         } else if (this.saldo > 500) {
             tipo = 'Platinum';
@@ -16,4 +15,6 @@ const cliente = {
     }
 }
 
-console.log(cliente.tipoCliente());
+const persona = new Cliente('Pedro', 2000);
+
+console.log(persona.tipoCliente());
