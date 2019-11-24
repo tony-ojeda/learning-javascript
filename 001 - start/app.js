61f1e3ee5b9aa5166d456694f719b7185c5a0f4d
tony_ojeda <tojeda96@gmai.com>
2019-11-24 11:41:48 -0500

    inherit prototypes in js

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -3,36 +3,23 @@ function Cliente(nombre, saldo) {
     this.saldo = saldo;
 }
 
-// Crear un prototype
-Cliente.prototype.tipoCliente = function() {
-    let tipo;
-    if (this.saldo > 1000) {
-        tipo = 'Gold';
-    } else if (this.saldo > 500) {
-        tipo = 'Platinum';
-    } else {
-        tipo = 'Normal';
-    }
-    return tipo;
-}
-
 // Prototipo que imprime saldo y nombre
 Cliente.prototype.nombreClienteSaldo = function() {
-    return `Nombre: ${this.nombre}, Tu saldo es de ${this.saldo}, 
-    Tipo Cliente ${this.tipoCliente()}`;
-}
-
-// Retirar Saldo
-Cliente.prototype.retirarSaldo = function(retiro) {
-    return this.saldo -= retiro;
+    return `Nombre: ${this.nombre}, Tu saldo es de ${this.saldo}`;
 }
 
 const cliente1 = new Cliente('Jose', 100);
-const cliente2 = new Cliente('David', 600);
-const cliente3 = new Cliente('Willy', 12000);
 
-cliente2.retirarSaldo(300);
+console.log(cliente1);
 
-console.log(cliente1.nombreClienteSaldo());
-console.log(cliente2.nombreClienteSaldo());
-console.log(cliente3.nombreClienteSaldo());
+
+// Banca para empresas
+function Empresa(nombre, saldo, telefono, tipo) {
+    Cliente.call(this, nombre, saldo);
+    this.telefono = telefono;
+    this.tipo = tipo;
+}
+
+Empresa.prototype = Object.create(Cliente.prototype);
+const empresa = new Empresa('EDTeam', 100000, 12234234, 'Eduacación');
+console.log(empresa.nombreClienteSaldo());
