e44158584990c8a50b659873a428682d353eb287
tony_ojeda <tojeda96@gmai.com>
2019-11-28 05:15:52 -0500

    ajax_json: show data persons in json

--- 008 -  ajax_json/app.js
+++ 008 -  ajax_json/app.js
@@ -1,5 +1,4 @@
 const boton1 = document.getElementById('boton1');
-
 boton1.addEventListener('click', () => {
     const xhr = new XMLHttpRequest();
 
@@ -18,6 +17,33 @@ boton1.addEventListener('click', () => {
                 </ul>
             `;
             document.getElementById('empleado').innerHTML = htmlTemplate;
+        }
+    }
+
+    xhr.send();
+})
+
+const boton2 = document.getElementById('boton2');
+boton2.addEventListener('click', () => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'empleados.json', true);
+
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            const personal = JSON.parse(xhr.responseText);
+            let htmlTemplate = '';
+            personal.forEach((persona) => {
+                htmlTemplate += `
+                <ul>
+                    <li>ID: ${persona.id}</li>
+                    <li>Nombre: ${persona.nombre}</li>
+                    <li>Empresa: ${persona.empresa}</li>
+                    <li>Actividad: ${persona.trabajo}</li>
+                </ul>
+                `;
+            })
+            document.getElementById('empleados').innerHTML = htmlTemplate;
         }
     }
 
