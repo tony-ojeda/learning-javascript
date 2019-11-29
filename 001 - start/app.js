88c3e8973d9422366d896b2fe41a5ffd7f02d136
tony_ojeda <tojeda96@gmai.com>
2019-11-29 06:10:00 -0500

    example callbacks

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,38 +1,21 @@
-// EMACScript6
-class Cliente {
-    constructor(nombre, saldo) {
-        this.nombre = nombre;
-        this.saldo = saldo;
-    }
+const paises = ['Francia', 'España', 'Portugal', 'Australia', 'Inglaterra', 'Irlanda'];
 
-    imprimirSaldo() {
-        return `Hola ${this.nombre}, tu saldo es de : ${this.saldo}`;
-    }
-
-    static bienvenida() {
-        return `Bienvenido al cajero`;
-    }
+function nuevoPais(nuevo, callback) {
+    setTimeout(() => {
+        paises.push(nuevo);
+        callback();
+    }, 3000)
 }
 
-// const tony = new Cliente('Pedro', 1000);
-// console.log(tony.imprimirSaldo());
-
-class Empresa extends Cliente {
-    constructor(nombre, saldo, telefono, tipo) {
-        // Va hacia el constructor pader
-        super(nombre, saldo);
-
-        // No existe en el constructor pader
-        this.telefono = telefono;
-        this.tipo = tipo;
-    }
-
-    static bienvenida() {
-        return `Bienvenido al cajero para Empresas`;
-    }
+function mostrarPaises() {
+    setTimeout(() => {
+        let html = '';
+        paises.forEach((pais) => {
+            html += `<li>${pais}</li>`;
+        })
+        document.getElementById('app').innerHTML = html;
+    }, 1000);
 }
 
-const empresa = new Empresa('EDTeam', 100000, 33535353, 'Construcción');
-
-console.log(Cliente.bienvenida());
-console.log(Empresa.bienvenida());
+nuevoPais('Perú', mostrarPaises);
+mostrarPaises();
