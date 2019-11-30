47e0111fbebfd3b4111575a1513110d49348653a
tony_ojeda <tojeda96@gmai.com>
2019-11-30 04:39:10 -0500

    async await

--- 001 - start/app.js
+++ 001 - start/app.js
@@ -1,15 +1,23 @@
-// Prmises 
-const aplicarDescuento = new Promise((resolve, reject) => {
-    const descuento = false;
-    if (descuento) {
-        resolve('Descuento Aplicado');
-    } else {
-        reject('No se puede aplicar descuento');
-    }
-});
+// Async Await
+async function obtenerClientes() {
+    // crear un pomise
+    const clientes = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Clientes descargados...');
+        }, 5000)
+    });
 
-aplicarDescuento.then((resultado) => {
-    console.log(resultado);
-}).catch((error) => {
-    console.log(error);
-})
+    // error o no...
+    const error = false;
+
+    if (!error) {
+        const respuesta = await clientes;
+        return respuesta;
+    } else {
+        await Promise.reject('Hubo un error...');
+    }
+}
+
+obtenerClientes()
+    .then(res => console.log(res))
+    .catch(error => console.log(error))
