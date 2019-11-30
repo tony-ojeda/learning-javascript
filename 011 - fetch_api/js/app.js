1ffb4577f970281fb5595adae768f6839440842c
tony_ojeda <tojeda96@gmai.com>
2019-11-29 21:31:48 -0500

    fetch_api: consume fetch api

--- 011 - fetch_api/js/app.js
+++ 011 - fetch_api/js/app.js
@@ -1,5 +1,6 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
 document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
+document.getElementById('apiBtn').addEventListener('click', cargarREST);
 
 function cargarTXT() {
     fetch('datos.txt')
@@ -31,4 +32,23 @@ function cargarJSON() {
         .catch((error) => {
             console.log(error);
         });
+}
+
+function cargarREST() {
+    fetch('https://picsum.photos/list')
+        .then((res) => {
+            return res.json();
+        })
+        .then((imagenes) => {
+            let html = '';
+            imagenes.forEach((imagen) => {
+                html += `
+                    <li>
+                        <a href="${imagen.post_url}" target="_blank" > Ver Imagen </a>
+                        ${imagen.author}
+                    </li>
+                `;
+            })
+            document.getElementById('resultado').innerHTML = html;
+        })
 }
