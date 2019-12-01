d952ba04d67a318f92b5fffbefd298ee89984025
tony_ojeda <tojeda96@gmai.com>
2019-12-01 06:06:21 -0500

    convert_money_cript: get input view

--- 012 - criptomonedas/js/app.js
+++ 012 - criptomonedas/js/app.js
@@ -1,3 +1,24 @@
 // instanciamos clases
 const cotizador = new Cotizador();
 const ui = new Interfaz();
+
+// obtener formulario
+const formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // leer moneda seleccionada
+    const monedaSelect = document.getElementById('moneda');
+    const monedaSeleccionada = monedaSelect[monedaSelect.selectedIndex].value;
+
+    // leer crito seleccionada
+    const criptoMonedaSelect = document.getElementById('criptomoneda');
+    const criptoMonedaSeleccionada = criptoMonedaSelect[criptoMonedaSelect.selectedIndex].value;
+
+    // comprobar que ambos campos tegand datos:
+    if (monedaSeleccionada === '' || criptoMonedaSeleccionada === '') {
+        console.log('Faltan datos');
+    } else {
+        console.log('cotizar');
+    }
+})
