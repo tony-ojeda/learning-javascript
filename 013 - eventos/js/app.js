919c456efade551a540521e1d7889f3f30e298d7
tony_ojeda <tojeda96@gmai.com>
2019-12-04 05:41:26 -0500

    event_local: save value inputs at view

--- 013 - eventos/js/app.js
+++ 013 - eventos/js/app.js
@@ -1,3 +1,22 @@
 // instanciar ambas clases
 const eventbrite = new EventBrite();
 const ui = new Interfaz();
+
+// listener al buscador
+document.getElementById('buscarBtn').addEventListener('click', e => {
+    e.preventDefault();
+
+    // leer el texto del input buscar
+    const textoBuscador = document.getElementById('evento').value;
+
+    // leer el select
+    const categorias = document.getElementById('listado-categorias');
+    const categoriaSeleccionada = categorias.options[categorias.selectedIndex].value;
+
+    // revisar que haya algo escrito en el buscador
+    if (textoBuscador !== '') {
+        console.log('Buscando');
+    } else {
+        console.log('no hay nada');
+    }
+})
