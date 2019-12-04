8da93fe1874a6a6437199db2d9b4442cf1485079
tony_ojeda <tojeda96@gmai.com>
2019-12-04 16:59:45 -0500

    project_code_faci: start practice

---
+++ 051-project_codi_faci/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlWebpack = new htmlWebpackPlugin({
+    template: './assets/index.template.html',
+    filename: 'index.html'
+})
+
+module.exports = {
+    entry: './assets/javascript/entry.js',
+    output: {
+        publicPath: '/',
+        path: path.join(__dirname, '..'),
+        filename: 'dist/javascript/bundle.js'
+    },
+    plugins: [htmlWebpack],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    }
+}
